8d919d44c27df4442656071ee412838626168f49
James <james.afonja@gmail.com>
2024-11-13 07:52:08 -0600

    Initial Commit

---
+++ NetworkingSample/NetworkingSampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  NetworkingSampleApp.swift
+//  NetworkingSample
+//
+//  Created by James Afonja on 11/13/24.
+//
+
+import SwiftUI
+
+@main
+struct NetworkingSampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
